7972893640ebf9a638d8a7296a66e34e3d045954
MaMaLiese <marlies.m@gmx.net>
2023-03-03 11:52:25 +0100

    repair (infinite) loop

---
+++ src/RepairLoop.java
@@ -0,0 +1,18 @@
+public class RepairLoop {
+    public static void main(String[] args) {
+//        int i = 1;
+//                do {
+//                    System.out.println(i);
+//                    i++;
+//                } while ( i == 1);
+
+
+        //man muss immer schaun, dass sich innerh. unseres Codeblocks etwas ändert
+        int b = 1;
+
+        while (b < 2) {
+            System.out.println(2);
+            b++;
+        }
+    }
+}
